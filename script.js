a27664d6265bb15baa4c794ebaae6cd8b62e5f86
delphinev <delphine.vilaysack@gmail.com>
2015-06-18 15:13:06 +0200

    ajout liste des topics

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
-    $http.get("http://www.w3schools.com/angular/customers.php")
-    .success(function(response) {$scope.names = response.records;});
+	$scope.ajax = function() {
+   		$http.get("http://vps164888.ovh.net/challengeangular/latest.json")
+    		.success(function(response) {
+			$scope.latest = response;
+			console.log($scope.latest);
+		});
+	};
+
+	$scope.show = function(id) {
+		
+		console.log(id);
+	};
 });
